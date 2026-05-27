9c1f5e3fb6c54675f393e6808dc567efdd312ac4
MLama2059 <lamamanish2059@gmail.com>
2026-05-27 16:00:04 +0545

    Add ApplicationUser for Identity

--- LibraryManagementSystem.Infrastructure/Data/ApplicationDbContext.cs
+++ LibraryManagementSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using LibraryManagementSystem.Domain.Entities;
+using LibraryManagementSystem.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -6,7 +8,7 @@ using System.Text;
 
 namespace LibraryManagementSystem.Infrastructure.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
         public DbSet<Book> Books { get; set; }
         public DbSet<Loan> Loans { get; set; }
@@ -29,6 +31,12 @@ namespace LibraryManagementSystem.Infrastructure.Data
                 entity.HasOne(l => l.Book)
                       .WithMany(b => b.Loans)
                       .HasForeignKey(l => l.BookId)
+                      .OnDelete(DeleteBehavior.Restrict);
+
+                // One ApplicationUser -> Many Loans
+                entity.HasOne<ApplicationUser>()
+                      .WithMany(u => u.Loans)
+                      .HasForeignKey(u => u.MemberId)
                       .OnDelete(DeleteBehavior.Restrict);
             });
         }
---
+++ LibraryManagementSystem.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagementSystem.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagementSystem.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; } = string.Empty;
+        public decimal AccountBalance { get; set; } = 0.00m;
+        public ICollection<Loan> Loans { get; set; } = new List<Loan>();
+    }
+}
