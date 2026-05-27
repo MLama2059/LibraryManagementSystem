44b31c4bc5326e3671eac43135220bf77f4157c1
MLama2059 <lamamanish2059@gmail.com>
2026-05-27 14:32:52 +0545

    Add Book and Loan entities

---
+++ LibraryManagementSystem.Domain/Entities/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagementSystem.Domain.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string ISBN { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public int TotalCopies { get; set; }
+        public int AvailableCopies { get; set; }
+        public ICollection<Loan> Loans { get; set; } = new List<Loan>();
+    }
+}
---
+++ LibraryManagementSystem.Domain/Entities/Loan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagementSystem.Domain.Entities
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public string MemberId { get; set; } = string.Empty;
+        public DateTime BorrowDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public bool IsReturned { get; set; }
+        public Book Book { get; set; } = null!;
+    }
+}
